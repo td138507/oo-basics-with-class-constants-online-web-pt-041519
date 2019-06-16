a092e9f7e8838358a2be01de50a89d7a49db53ac
Taylor Doyle <tcd56@cornell.edu>
2019-06-16 04:14:10 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :material, :condition, :size
   attr_reader :brand
+
+BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
+    BRANDS.uniq!
   end
 
   def cobble
